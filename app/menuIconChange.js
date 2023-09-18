dc8704372f870268f4d5117cf197a530913ba0b5
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-09-18 10:23:23 -0300

    On scroll animations

--- app/menuIconChange.js
+++ app/menuIconChange.js
@@ -1,15 +1,15 @@
 /* pega o tamanho da tela e o conteúdo do menu */
-const windowSize = window.innerWidth
-const menuTxt = document.querySelectorAll(".menu-item")
+const windowSize = window.innerWidth;
+const menuTxt = document.querySelectorAll(".menu-item");
 
 /* se a tela for menor ou igual a 768px muda o conteúdo do menu para ícones do Fonts Awesome */
 function menuItems() {
   if (windowSize <= 768) {
-    menuTxt[0].innerHTML = '<i class="fa-solid fa-user"></i>'
-    menuTxt[1].innerHTML = '<i class="fa-solid fa-code"></i>'
-    menuTxt[2].innerHTML = '<i class="fa-solid fa-sliders"></i>'
-    menuTxt[3].innerHTML = '<i class="fa-sharp fa-solid fa-bars"></i>'
+    menuTxt[0].innerHTML = '<i class="fa-solid fa-user"></i>';
+    menuTxt[1].innerHTML = '<i class="fa-solid fa-code"></i>';
+    menuTxt[2].innerHTML = '<i class="fa-solid fa-sliders"></i>';
+    menuTxt[3].innerHTML = '<i class="fa-sharp fa-solid fa-bars"></i>';
   }
 }
 
-export default menuItems
+export default menuItems;
